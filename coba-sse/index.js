bf79c8ee3612dd3c26006336e0ea19489b1e279a
nathaniaelirica <67095933+nathaniaelirica@users.noreply.github.com>
2023-06-15 16:30:25 +0700

    sse

---
+++ coba-sse/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+const port = 3030;
+
+// Define route
+app.get("/", (_req, res) => {
+	console.log("Client connected");
+
+	res.setHeader("Content-Type", "text/event-stream");
+	res.setHeader("Access-Control-Allow-Origin", "*");
+
+	const intervalId = setInterval(() => {
+		const date = new Date().toLocaleString();
+		res.write(`data: ${date}\n\n`);
+	}, 10000);
+
+	res.on("close", () => {
+		console.log("Client closed connection");
+		clearInterval(intervalId);
+		res.end();
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Server is running`);
+});
